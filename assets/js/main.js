0aeef594e194aabd0eb8cd78f93eed4d2d167416
dv-patrickoliveira <patrick.oliveirasouza@hotmail.com>
2021-10-19 21:22:18 -0300

    organização commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,16 +9,19 @@ const clock = () =>{
     let hh = date.getHours() * 30,
         mm = date.getMinutes() * 6,
         ss = date.getSeconds() * 6
-
+        
+    
     hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
     minutes.style.transform = `rotateZ(${mm}deg)`
     seconds.style.transform = `rotateZ(${ss}deg)`
 }
-setInterval(clock, 1000) 
+setInterval(clock, 1000) // 1000 = 1S
+
 
 const textHour = document.getElementById('text-hour'),
       textMinutes = document.getElementById('text-minutes'),
       textAmPm = document.getElementById('text-ampm'),
+    
       dateDay = document.getElementById('date-day'),
       dateMonth = document.getElementById('date-month'),
       dateYear = document.getElementById('date-year')
@@ -30,51 +33,73 @@ const clockText = () =>{
         ampm,
         mm = date.getMinutes(),
         day = date.getDate(),
+        
         month = date.getMonth(),
         year = date.getFullYear()
 
-    if(hh <= 12){
+    
+    if(hh >= 12){
         hh = hh - 12
         ampm = 'PM'
     }else{
         ampm = 'AM'
     }
 
+    
     if(hh == 0){hh = 12}
 
+    
     if(hh < 10){hh = `0${hh}`}
 
+   
     textHour.innerHTML = `${hh}:`
-
+    
+    
     if(mm < 10){mm = `0${mm}`}
-
+    
+    
     textMinutes.innerHTML = mm
+
+    
     textAmPm.innerHTML = ampm
 
+    
     let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+    
     dateDay.innerHTML = day
+    
     dateMonth.innerHTML = `${months[month]},`
     dateYear.innerHTML = year
 }
-setInterval(clockText, 1000) // 1000 = 1s
+setInterval(clockText, 1000) // 1000 = 1S
+
 
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bxs-sun'
 
+
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
+
 
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun'
 
+
 if (selectedTheme) {
+
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'bxs-moon' ? 'add' : 'remove'](iconTheme)
 }
+
+
 themeButton.addEventListener('click', () => {
+    
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
+    
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
